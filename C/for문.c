530482d47947e34def66730e4b9527fcfeb61b9c
송성민 <seungmin6610@naver.com>
2026-08-24 10:24:25 +0900

    update

--- C/for문.c
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int main()
-{
-	int i;
-	for (i = 1; i <= 10; i = i + 1)
-		printf("i = %d\n", i);
-}
--- C/for문2.c
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int main()
-{
-	for (int i = 50; i < 100; i++)
-		if(!(i%5))printf("i=%d\n", i);
-}
